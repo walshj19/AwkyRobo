7e6b2194ab432596e0e5271d66ef96d51cbfd0bc
walshj19 <walshj19@tcd.ie>
2014-07-07 23:51:16 +0100

    Changed to osx libs

--- EclipseProjcet/src/Main.java
+++ EclipseProjcet/src/Main.java
@@ -1,10 +1,14 @@
 import processing.core.*;
 
 public class Main extends PApplet{
+	  int locationX = 100;
+	  int locationY = 100;
+	  int size = 50;
+	  int speed = 0;
+	  
 	public void setup() {
 		size(800,800, P3D);
 		background(0);
-		
 		
 		//right
 		//pushMatrix();
@@ -27,9 +31,33 @@ public class Main extends PApplet{
 		stroke(255);
 		//back
 		pushMatrix();
+
 		translate(200, 200, 0);
+		stroke(random(255), random(255), random(255));
+
+	    float x1 = random(size);
+	    float y1 = random(size);
+
+	    //l1 -> l2 is horizontal
+	    float x2 = x1;
+	    float y2 = random(size);
+
+	    float x3 = random(size);
+	    float y3 = y2;
+
+	    float x4 = x3;
+	    float y4 = random(size);
+
+	    float x5 = random(size);
+	    float y5 = y4;
+
+	    line(locationX+x1, locationY+y1, locationX+x2, locationY+y2);
+	    line(locationX+x2, locationY+y2, locationX+x3, locationY+y3);
+	    line(locationX+x3, locationY+y3, locationX+x4, locationY+y4);
+	    line(locationX+x4, locationY+y4, locationX+x5, locationY+y5);
+	    
 		//rect(0, 0, 200, 200);
-		for(int i = 0; i < 10; i++){
+		/*for(int i = 0; i < 10; i++){
 			pushMatrix();
 			translate(0, i*20, 0);
 			for(int j = 0; j < 10; j++){
@@ -39,7 +67,7 @@ public class Main extends PApplet{
 				popMatrix();
 			}
 			popMatrix();
-		}
+		}*/
 		popMatrix();
 		//left
 		pushMatrix();
@@ -81,3 +109,30 @@ public class Main extends PApplet{
 		PApplet.main(new String[] { "--present", "Main" });
 	}*/
 }
+/*
+class Entity {
+
+	  void drawEntity() {
+	    
+	  }
+
+	  void changeLocation(int x, int y) {
+	    locationX += x;
+	    locationY += y;
+	  }
+
+	  void moveRandom() {
+	    int tx = locationX + (int)random(-speed, speed);
+	    if (tx >= 0 && tx <= 800) {
+	      locationX = tx;
+	    }
+	    int ty = locationY + (int)random(-speed, speed);
+	    if (ty >= 0 && ty <= 800) {
+	      locationY = ty;
+	    }
+	  }
+
+	  void changeSpeed(int d) {
+	    speed += d;
+	  }
+	}*/
